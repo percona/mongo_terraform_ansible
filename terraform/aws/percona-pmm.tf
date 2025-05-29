996d2954a22ce5ba34ad1761e1a4ebad73a132fd
igroene <9805809+igroene@users.noreply.github.com>
2025-05-29 09:10:07 -0300

    ebs volume race condition, fixes

--- terraform/aws/percona-pmm.tf
+++ terraform/aws/percona-pmm.tf
@@ -29,6 +29,12 @@ resource "aws_instance" "pmm" {
     # Update /etc/hosts to reflect the hostname change
     echo "127.0.0.1 $(hostname)" > /etc/hosts  
 
+    DEVICE="/dev/nvme1n1"
+    while [ ! -b "$DEVICE" ]; do
+      echo "Waiting for $DEVICE to be attached..."
+      sleep 2
+    done
+    
     # Add a dash to lsblk output to match the Terraform volume ID 
     DEVICE=$(lsblk -o NAME,SERIAL | sed 's/l/l-/' | grep "${aws_ebs_volume.pmm_disk.id}" | awk '{print "/dev/" $1}')
 

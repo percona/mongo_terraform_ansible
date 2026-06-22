d2a46f849aeacabf999fa9267310b8000784c940
radoslawszulgo <radoslaw.szulgo@percona.com>
2026-06-22 14:12:19 +0200

    Implement mongot support

--- terraform/chaos/outputs.tf
+++ terraform/chaos/outputs.tf
@@ -45,6 +45,8 @@ resource "local_file" "AnsibleInventoryCluster" {
       pbm_release        = var.pbm_release
       pbm_version        = var.pbm_version
       pmm_client_version = var.pmm_client_version
+      enable_mongot      = var.enable_mongot
+      mongot_node_count  = var.mongot_node_count
     }
   )
 
@@ -113,6 +115,8 @@ resource "local_file" "AnsibleInventoryRS" {
       pbm_release        = var.pbm_release
       pbm_version        = var.pbm_version
       pmm_client_version = var.pmm_client_version
+      enable_mongot      = var.enable_mongot
+      mongot_node_count  = var.mongot_node_count
     }
   )
 
